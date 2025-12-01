f75707ef5094db8323c4954af3412b08aaa3a42b
Astin-Salivio <justin.salivio2580@gmail.com>
2025-12-01 19:58:23 +0800

    TestV2

--- UIScriptV1.js
+++ UIScriptV1.js
@@ -1,3 +1,9 @@
-document.getElementById("darkToggle").addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode");
+document.addEventListener("DOMContentLoaded", () => {
+    const obj = document.getElementById("darkToggle");
+
+    obj.addEventListener("load", () => {
+        obj.contentDocument.documentElement.addEventListener("click", () => {
+            document.body.classList.toggle("dark-mode");
+        });
+    });
 });
